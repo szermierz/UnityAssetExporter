bc49f0dd44ef62c7d8b9c4c3219f40e93ecebb15
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2018-04-09 00:07:16 +0200

    [RelationRules] Added rule base implementation

--- RelationRules/AssetRelationRuleBase.cs
+++ RelationRules/AssetRelationRuleBase.cs
@@ -1,16 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssetRelationRuleBase : MonoBehaviour {
+public class AssetRelationRuleBase
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public readonly Type MatchingObjectType;
+
+    public AssetRelationRuleBase(Type matchingObjectType)
+    {
+        MatchingObjectType = matchingObjectType;
+    }
+
+    public delegate void ObjectMatchedDelegate(UnityEngine.Object unityObject);
+
+    public ObjectMatchedDelegate OnObjectMatched;
+
+    /* Template method design pattern */
+    public virtual bool VerifyObject(UnityEngine.Object unityObject)
+    {
+        if(!VeryfyObjectType(unityObject))
+            return false;
+
+        var relatedAssets = TryFindRelatedAssets(unityObject);=
+        NotifyRelationFound(relatedAssets);
+
+        return true;
+    }
+
+    protected virtual void NotifyRelationFound(List<UnityEngine.Object> relatedAssets)
+    {
+        if(null == relatedAssets || null == OnObjectMatched)
+            return;
+
+        foreach(var relatedAsset in relatedAssets)
+            OnObjectMatched.Invoke(relatedAsset);
+    }
+
+    protected virtual bool VeryfyObjectType(UnityEngine.Object unityObject)
+    {
+        return MatchingObjectType.IsAssignableFrom(unityObject.GetType());
+    }
+
+    protected virtual List<UnityEngine.Object> TryFindRelatedAssets(UnityEngine.Object unityObject)
+    {
+        return new List<UnityEngine.Object>();
+    }
+
 }
