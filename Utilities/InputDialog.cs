34ecdfb893bc0adb821b82cc61312f0a08ad0b73
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2018-04-08 23:10:06 +0200

    [Repo] Initialized empty files

---
+++ Utilities/InputDialog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDialog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+//using UnityEngine;
+//using UnityEditor;
+
+//public class ShowPopupExample : EditorWindow
+//{
+//    [MenuItem("Example/ShowPopup Example")]
+//    static void Init()
+//    {
+//        ShowPopupExample window = ScriptableObject.CreateInstance<ShowPopupExample>();
+//        window.position = new Rect(Screen.width / 2, Screen.height / 2, 250, 150);
+//        window.ShowPopup();
+//    }
+
+//    void OnGUI()
+//    {
+//        EditorGUILayout.LabelField("This is an example of EditorWindow.ShowPopup", EditorStyles.wordWrappedLabel);
+//        GUILayout.Space(70);
+//        if(GUILayout.Button("Agree!")) this.Close();
+//    }
+//}
