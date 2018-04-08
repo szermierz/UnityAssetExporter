94feea366752f7073ef1ea126be586e64558a0bb
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2018-04-08 23:56:52 +0200

    [Core] Added basic core implementation

--- Core/AssetDefinition.cs
+++ Core/AssetDefinition.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class AssetDefinition : MonoBehaviour {
+namespace AssetsExporting
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public class AssetDefinition
+    {
+
+        public string AssetPath { get; protected set; }
+
+        protected List<AssetDefinition> m_RelatedAssets = new List<AssetDefinition>();
+        public List<AssetDefinition> RelatedAssets { get { return m_RelatedAssets; } }
+
+        public AssetDefinition(string assetPath)
+        {
+            AssetPath = assetPath;
+        }
+
+        public void AddRelatedAsset(AssetDefinition assetDefinition)
+        {
+            RelatedAssets.Add(assetDefinition);
+        }
+
+        public string Extension
+        { get { return Path.GetExtension(AssetPath); } }
+
+        public UnityEngine.Object Asset
+        { get { return UnityEditor.AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(AssetPath); } }
+
+        public override string ToString()
+        {
+            return AssetPath;
+        }
+
+        public virtual void FromString(string value)
+        {
+            AssetPath = value;
+        }
+
+    }
+
 }
+
--- Core/AssetDefinitionSerializer.cs
+++ Core/AssetDefinitionSerializer.cs
@@ -1,16 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class AssetDefinitionSerializer : MonoBehaviour {
+namespace AssetsExporting
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public class AssetDefinitionSerializationBase
+    {
+
+        public readonly string FilePath;
+
+        public AssetDefinitionSerializationBase(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        protected List<AssetDefinition> m_AssetDefinitions = new List<AssetDefinition>();
+
+        protected readonly string c_AssetSeparator = "\n";
+
+    }
+
+    public class AssetDefinitionSerializer : AssetDefinitionSerializationBase
+    {
+
+        public AssetDefinitionSerializer(string filePath) : base(filePath)
+        { }
+
+        public void AddAssetDefinition(AssetDefinition assetDefinition)
+        {
+            m_AssetDefinitions.Add(assetDefinition);
+        }
+
+        public void Serialize()
+        {
+            foreach(var assetDefinition in m_AssetDefinitions)
+                File.AppendText(assetDefinition.ToString() + c_AssetSeparator);
+        }
+
+    }
+
+    public class AssetDefinitionDeserializer : AssetDefinitionSerializationBase
+    {
+
+        public AssetDefinitionDeserializer(string filePath) : base(filePath)
+        { }
+
+        public IEnumerator<AssetDefinition> Deserialize()
+        {
+            var fileContent = File.ReadAllText(FilePath);
+            var assetPaths = fileContent.Split(new string[] { c_AssetSeparator }, System.StringSplitOptions.RemoveEmptyEntries);
+
+            m_AssetDefinitions.Clear();
+
+            foreach(var assetPath in assetPaths)
+                m_AssetDefinitions.Add(new AssetDefinition(assetPath));
+
+            return m_AssetDefinitions.GetEnumerator();
+        }
+
+    }
+
 }
